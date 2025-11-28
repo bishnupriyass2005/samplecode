a7dfdf33444ee4a662b41db7c04d29d0a749ee7b
bishnupriyass2005 <bishnupriya.ss2005@gmail.com>
2025-11-28 10:05:31 +0000

    feat:Add database connectivity

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 from http.server import HTTPServer
 from router import StudentRouter
+from database.connection import init_database
 
-def run_server(port=8000):
+def run_server():
 
-    server = HTTPServer(("", port), StudentRouter)
-    print(f" Server running at http://localhost:{port}")
+    server = HTTPServer(("", 8000), StudentRouter)
+    print(f" Server running at http://localhost:{8000}")
     server.serve_forever()
 
     if __name__ == "__main__":
--- router.py
+++ router.py
@@ -1,19 +1,30 @@
+# Matches incoming HTTP paths (URLs) to handlers (like a tiny Express/Django router)
+
+from datetime import datetime
+from http.server import BaseHTTPRequestHandler
+from urllib.parse import urlparse
+
+from controllers.students import (
+    get_all_students
+)
+
+from core.static import serve_static
+from core.responses import send_404
+from core.middleware import add_cors_headers
+
+
 class StudentRouter(BaseHTTPRequestHandler):
 
-    # def do_GET(self):
-    #     path = urlparse(self.path).path
+    def do_OPTIONS(self):
+          self.send_response(200)
+          add_cors_headers(self)
+          self.end_headers()
 
-    #     if path in ("/", "/index.html"):
-    #         return serve_html(self)
-        
-    #     if path.startswith("/static/"):
-    #         return serve_static(self)
-        
+    def do_GET(self):
+        path = urlparse(self.path).path
+
         if path == "/api/students":
             return get_all_students(self)
         
-        # if path.startswith("/api/students/"):
-        #     student_id = int(path.split("/")[-1])
-        #     return get_student(self, student_id)
-        
+       
         return send_404(self)
